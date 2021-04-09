a22470c664e6c64e64ad154ba1ce1b1bc3c1478b
Taromaru <48397556+Taromaruu@users.noreply.github.com>
2021-04-09 13:11:06 -0400

    Initial Commit

---
+++ test.py
@@ -0,0 +1,34 @@
+import doggomenus
+
+x = 0
+
+def atx():
+    global x
+    x += 1
+
+def aatx():
+    am = input("Amount > ")
+
+    try:
+        am = int(am)
+    except ValueError:
+        print("Not a number!")
+        return
+
+    global x
+    x += am
+    print("Done!")
+
+def stuck():
+    return
+
+var1 = doggomenus.Command("Add +1 to x", atx)
+var2 = doggomenus.Command("Add a amount to x", aatx, pause=True)
+showvar = doggomenus.Command("Show x", lambda: print(x), pause=True)
+menuvar = doggomenus.SelectionMenu(var1, var2, showvar, title="Variable Testing")
+
+stuckcmd = doggomenus.Command("You are stuck... Have fun :)", stuck)
+stuckmenu = doggomenus.SelectionMenu(stuckcmd, title="???", exit_cmd=False)
+
+menu = doggomenus.SelectionMenu(menuvar, stuckmenu, title="Example menu")
+menu.run()
